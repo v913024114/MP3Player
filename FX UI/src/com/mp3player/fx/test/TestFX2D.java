cbadcb6e2302971662ab90c0c0b46c8a86bf4e1a
Philipp <Philipp@MHoll.de>
2017-02-12 18:10:13 +0100

    Added FXML for basic player UI

--- FX UI/src/com/mp3player/fx/test/TestFX2D.java
+++ FX UI/src/com/mp3player/fx/test/TestFX2D.java
@@ -1,20 +1,18 @@
 package com.mp3player.fx.test;
 
 import com.aquafx_project.AquaFx;
-import com.mp3player.fx.CircularSlider;
 import com.mp3player.fx.FileDropOverlay;
 import com.mp3player.fx.PlayerControl;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class TestFX2D extends Application {
 	private Scene scene;
@@ -22,9 +20,11 @@ public class TestFX2D extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-//		CircularSlider slider = new CircularSlider();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("mp3player.fxml"));
+		loader.setController(this);
+		BorderPane root = loader.load();
+		root.setCenter(new PlayerControl());
 
-		BorderPane root = new BorderPane(new PlayerControl());
 		new FileDropOverlay(root);
 		primaryStage.setScene(scene = new Scene(root));
 		scene.getStylesheets().add(getClass().getResource("defaultstyle.css").toExternalForm());
@@ -32,31 +32,25 @@ public class TestFX2D extends Application {
 
 		Slider realSlider = new Slider(0, 1, 50);
 		realSlider.setShowTickMarks(true);
-		root.setBottom(realSlider);
 
 
 //		new Timeline(new KeyFrame(Duration.ZERO, new KeyValue(slider.valueProperty(), 0)),
 //				new KeyFrame(new Duration(10000), new KeyValue(slider.valueProperty(), 99))).play();
 
-//		slider.setDuration(3.4*60);
-
-
-		new Thread(() -> {
-			try {
-				Thread.sleep(4000);
-				Platform.runLater(() -> {
-//					setUserAgentStylesheet(STYLESHEET_CASPIAN);
-//					AquaFx.style();
-//					scene.getStylesheets().add(getClass().getResource("dark.css").toExternalForm());
-				});
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}).start();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	@FXML
+	public void setStyle(ActionEvent e) {
+		MenuItem item = (MenuItem) e.getTarget();
+		String text = item.getText();
+		if(text.equals("AquaFX")) {
+			AquaFx.style();
+		} else {
+			setUserAgentStylesheet(text.toUpperCase());
+		}
 	}
 }
