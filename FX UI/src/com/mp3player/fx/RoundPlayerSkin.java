b9d1cfdce60f8613369ba0b4dd33eef7db2b14f7
Philipp Holl <pholl@mpp.mpg.de>
2017-02-22 10:40:01 +0100

    Button icons now scale with window size

--- FX UI/src/com/mp3player/fx/RoundPlayerSkin.java
+++ FX UI/src/com/mp3player/fx/RoundPlayerSkin.java
@@ -1,6 +1,8 @@
 package com.mp3player.fx;
 import com.mp3player.fx.icons.FXIcons;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Bounds;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
@@ -30,10 +32,15 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
 	private Button stopButton, prevButton, nextButton, listButton, searchButton;
 	private double innerMargin = 2, outerMargin = 5;
 	private ImageView playIcon, pauseIcon;
+	private DoubleProperty heightScale;
 
 
 	public RoundPlayerSkin(PlayerControl control) {
 		super(control);
+
+		heightScale = new SimpleDoubleProperty(1);
+//		heightScale.bind(Bindings.min(getSkinnable().widthProperty(), getSkinnable().heightProperty()).divide(1/300.0));
+
 		createSlider();
 		createButtons();
 	}
@@ -73,8 +80,8 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
 		buttonRoot = new Group(buttonPane);
 		getChildren().add(buttonRoot);
 
-		playIcon = FXIcons.get("Play.png", 32);
-		pauseIcon = FXIcons.get("Pause.png", 32);
+		playIcon = FXIcons.get("Play.png", 32, heightScale);
+		pauseIcon = FXIcons.get("Pause.png", 32, heightScale);
 		buttonPane.setCenter(playButton = new ToggleButton(null, playIcon));
 		playButton.selectedProperty().bindBidirectional(getSkinnable().playingProperty());
 		getSkinnable().playingProperty().addListener((p,o,n) -> {
@@ -88,10 +95,10 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
 		BorderPane.setAlignment(bottomRoot, Pos.TOP_CENTER);
 
 		Insets buttonMargins = new Insets(innerMargin);
-		bottomButtonPane.setCenter(stopButton = new Button(null, FXIcons.get("Stop.png", 20)));
-		bottomButtonPane.setLeft(prevButton = new Button(null, FXIcons.get("Previous.png", 32)));
+		bottomButtonPane.setCenter(stopButton = new Button(null, FXIcons.get("Stop.png", 20, heightScale)));
+		bottomButtonPane.setLeft(prevButton = new Button(null, FXIcons.get("Previous.png", 32, heightScale)));
 		BorderPane.setAlignment(prevButton, Pos.TOP_RIGHT);
-		bottomButtonPane.setRight(nextButton = new Button(null, FXIcons.get("Next.png", 32)));
+		bottomButtonPane.setRight(nextButton = new Button(null, FXIcons.get("Next.png", 32, heightScale)));
 		BorderPane.setMargin(playButton, buttonMargins);
 		BorderPane.setMargin(stopButton, buttonMargins);
 		BorderPane.setMargin(prevButton, buttonMargins);
@@ -122,19 +129,19 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
 		});
 
 		// Loop
-		getChildren().add(loopButton = new ToggleButton(null, FXIcons.get("Loop.png", 24)));
+		getChildren().add(loopButton = new ToggleButton(null, FXIcons.get("Loop.png", 24, heightScale)));
 		loopButton.selectedProperty().bindBidirectional(getSkinnable().loopProperty());
 		loopButton.setTooltip(new Tooltip("Loop playlist"));
 
 		// Shuffle
-		getChildren().add(shuffleButton = new ToggleButton(null, FXIcons.get("Shuffle.png", 24)));
+		getChildren().add(shuffleButton = new ToggleButton(null, FXIcons.get("Shuffle.png", 24, heightScale)));
 		shuffleButton.selectedProperty().bindBidirectional(getSkinnable().shuffledProperty());
 		shuffleButton.setTooltip(new Tooltip("Shuffled playlist\n"
 				+ "When active: modifying playlist will trigger playlist to be shuffled\n"
 				+ "On activation: shuffle playlist"));
 
 		// Playlist
-		getChildren().add(listButton = new Button(null, FXIcons.get("Playlist.png", 24)));
+		getChildren().add(listButton = new Button(null, FXIcons.get("Playlist.png", 24, heightScale)));
 		listButton.setOnAction(e -> {
 			if(getSkinnable().getOnShowPlaylist() != null)
 				getSkinnable().getOnShowPlaylist().handle(e);
@@ -142,7 +149,7 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
 		listButton.setTooltip(new Tooltip("Show playlist"));
 
 		// Search
-		getChildren().add(searchButton = new Button(null, FXIcons.get("Search.png", 24)));
+		getChildren().add(searchButton = new Button(null, FXIcons.get("Search.png", 24, heightScale)));
 		searchButton.setOnAction(e -> {
 			if(getSkinnable().getOnSearch() != null)
 				getSkinnable().getOnSearch().handle(e);
@@ -186,6 +193,9 @@ public class RoundPlayerSkin extends SkinBase<PlayerControl>
             double contentWidth, double contentHeight) {
     	double barRadius = Math.min(contentWidth, contentHeight) * slider.getDiameter() / 2;
     	double barWidth = Math.min(contentWidth, contentHeight) * slider.getThickness();
+
+    	heightScale.set(barRadius / 120);
+
 		layoutButtons((barRadius-barWidth/2) - outerMargin);
     	layoutInArea(slider, contentX, contentY, contentWidth, contentHeight, 0, HPos.CENTER, VPos.CENTER);
     	layoutInArea(buttonRoot, contentX, contentY, contentWidth, contentHeight, 0, HPos.CENTER, VPos.CENTER);
